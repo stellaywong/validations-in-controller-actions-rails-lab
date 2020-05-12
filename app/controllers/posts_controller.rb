8d53ae557fbf3e8210307ad4a72b2ef431423fc0
stellaywong <stellayinwong@gmail.com>
2020-05-11 22:33:20 -0400

    Validations in models, renders in controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    set_post!
   end
 
   def update
-    @post.update(post_params)
+    set_post!
 
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
   end
 
   private
